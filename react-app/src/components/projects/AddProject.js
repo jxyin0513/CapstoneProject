ca32352572f2df465b5d6c18361c2e6651f8f9ed
Jingxun Yin <jingxunyin1@gmail.com>
2022-08-03 13:34:56 -0400

    project CRUD init

---
+++ react-app/src/components/projects/AddProject.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {useHistory} from 'react-router-dom'
+import { CreateProjects } from '../../store/projects';
+
+function NewProject(){
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const user = useSelector(state=>state.session.user)
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('');
+    const [errors, setErrors] = useState([])
+    async function onSubmit(e){
+        e.preventDefault();
+        const project = {
+            userId: user.id,
+            name,
+            description
+        }
+        const newProject = await dispatch(CreateProjects(project))
+        if(!newProject){
+            history.push('/')
+        }else{
+            setErrors(newProject)
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <div>
+                    {errors.map((error, ind) => (
+                    <div key={ind}>{error}</div>
+                    ))}
+                </div>
+                <label>Project Name
+                    <input type='text' name='name' onChange={e=>setName(e.target.value)}></input>
+                </label>
+                <label>Description
+                    <input type='text' name='description' onChange={e=>setDescription(e.target.value)}></input>
+                </label>
+                <button type='submit'>Add Project</button>
+            </form>
+        </>
+    )
+}
+
+export default NewProject;
---
+++ react-app/src/components/projects/ProjectDetail.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+function Project(){
+    const project = useSelector(state=>state.projects)
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Project;
