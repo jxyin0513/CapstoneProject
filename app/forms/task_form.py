086a7e2618ec0f80348d21e450936562e0c056b1
Jingxun Yin <jingxunyin1@gmail.com>
2023-04-04 23:29:26 -0400

    fixed few designs

--- app/forms/task_form.py
+++ app/forms/task_form.py
@@ -11,16 +11,14 @@ def date_check(form, field):
         raise ValidationError('Task deadline must be in future date.')
     if(field.data >= project.deadline):
         raise ValidationError('Task deadline must be before project deadline')
-    print(form.data['projectId'])
-    print(field.data)
 
 class TaskForm(FlaskForm):
     projectId = IntegerField('projectId', validators=[DataRequired(message="Please provide the project Id.")])
     userId = IntegerField('userId', validators=[DataRequired(message='Please provide your user Id.')])
+    sectionId = IntegerField('sectionId', validators=[DataRequired(message='Please choose your section.')])
     assignee = StringField('assignee', validators=[DataRequired(message='Please provide name of the assignee.'), Length(min=1, max=20, message='Message should be contrained to 20 characters.')])
     taskName = StringField('name', validators=[DataRequired(message='Please provide your task name.'), Length(max=30, message='Please restrain your task name to 30 characters.')])
     status = StringField('status', validators=[DataRequired(message='Please provide current status of your task')])
     startdate = DateField('startdate', validators=[DataRequired(message='Start date must be provided')])
     deadline = DateField('deadline', validators=[DataRequired(message='Deadline must be provided'), date_check])
     priority = StringField('priority', validators=[DataRequired(message='Please provide your priority'), Length(min=1, message="Please choose your priority")])
-    sectionId = IntegerField('sectionId', validators=[DataRequired(message='Please choose your section.')])
