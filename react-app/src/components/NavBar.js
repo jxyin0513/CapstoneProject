eb70056b41a7731e08027fdab1de571b8281a4d4
Jingxun Yin <jingxunyin1@gmail.com>
2023-05-11 23:36:59 -0400

    fixed some design

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -15,9 +15,7 @@ const NavBar = () => {
           <NavLink to='/' exact={true} activeClassName='home-Logo'>
             <img src={logo} className='logo' height='50px' width='auto' alt='Site Logo'></img>
           </NavLink>
-          {/* {user &&
-            <LogoutButton />
-          } */}
+
         </div>
 
         {!user && (
