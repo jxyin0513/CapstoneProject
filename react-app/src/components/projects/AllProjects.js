907be063d98de232509be8ff6b12f447709ac8e0
Jingxun Yin <jingxunyin1@gmail.com>
2023-06-21 23:19:48 -0400

    fixed few errs

--- react-app/src/components/projects/AllProjects.js
+++ react-app/src/components/projects/AllProjects.js
@@ -17,7 +17,6 @@ function AllProjects(){
     return(
         <>
             <h3 className='user-Name'>@{user.username}</h3>
-
             {allProjects && (allProjects.map(project=>(
                 <NavLink activeClassName={'project-detail'} className='project-detail-nav-Bar' key={project.id} to={`/projects/${project.id}`}>
                     <div className='project-detail-Name'>{project.name}</div>
